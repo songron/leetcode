b2248fbd7224d61c3a2ba60faa2a6f9109b49b12
Muhammad Arham Jan <146324130+ArhamJan19@users.noreply.github.com>
2024-03-19 19:12:56 +0500

    Arhams Updated version of anagrams.cpp
    Updated the existing file anagrams.cpp

--- c++/anagrams.cpp
+++ c++/anagrams.cpp
@@ -1,29 +1,55 @@
-class Solution {
-public:
-    vector<string> anagrams(vector<string> &strs) {
-        /* https://oj.leetcode.com/problems/anagrams/
-        ["abc", "ef", "cba", "xy", "yx"]  ->  ["abc", "cba", "xy", "yx"]
-        the order doesn't matter
-        */
-        
-        vector<string> result;
-        unordered_map<string, string> dict;
-        
-        for (int i = 0; i < strs.size(); i++) {
-            string s = strs[i];
-            sort(s.begin(), s.end());
-            if (dict.find(s) == dict.end()) {
-                dict[s] = strs[i];
-            }
-            else {
-                if (dict[s] != "#") {
-                    result.push_back(dict[s]);
-                    dict[s] = "#";
-                }
-                result.push_back(strs[i]);
-            }
-        }
-        
-        return result;
-    }
-};
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string first = "";
+	string second = "";
+	cout << "\nEnter the First Word: ";
+	getline(cin, first);
+	cout << "\nEnter the Second Word: ";
+	getline(cin, second);
+	int letters_counter=0;
+	if (first.length() == second.length())
+	{
+		bool found = false;
+		
+		for (int i = 0; i < first.length(); i++)
+		{
+			for (int j = 0; j < second.length(); j++)
+			{
+				if (first[i] == second[j])
+				{
+					found = true;
+					break;
+				}
+
+			}
+
+			if (found)
+			{
+				letters_counter += 1;
+			}
+		}
+		if (letters_counter == first.length())
+		{
+			cout << "\nANAGRAMS";
+		}
+		else
+		{
+			cout << "\nNOT ANAGRAMS";
+		}
+
+	}
+	else
+	{
+		cout << "\nNOT ANAGRAMS";
+		cout << endl << endl;
+		system("pause");
+		return 0;
+	}
+	
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
